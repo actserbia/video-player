18f471e2d1d0dd1681cac5b3d713037fa06707db
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2018-03-14 12:13:15 +0100

    kaltura promise

--- src/main.js
+++ src/main.js
@@ -1,44 +1,21 @@
+/*
+babel-polyfill, core-js
+*/
+
 import style from './main.scss'
 import devStyle from './layout.scss.dev'
 
 import getKalturaData from './kaltura/get-data.js'
 
-let domVids = document.getElementsByTagName('video')
 
-let vids = Array.from(domVids);
-
-console.log( domVids )
-
-console.log( [...domVids] )
-
-console.log( vids )
-
-
-
-/*
-let vids = document.getElementsByTagName('video');
-for(i=0; i < vids.lenght; i++){
-  let vid = vids[i];
-
+const resolveKaltura = function(data) {
+  console.log('resolveKalture', data);
 }
 
-
-Array.prototype.forEach.call(, function(e){
-  console.log(e);
+// ITERATE PAGE VIDEOS
+let htmlVideos = document.getElementsByTagName('video');
+[...htmlVideos].forEach(function(video, index){
+  if ( video.classList.contains('video-player-kaltura') ){
+    getKalturaData(video.dataset.vid).then(resolveKaltura);//dataset
+  };
 });
-
-//Array.from( document.getElementsByTagName('video') ).forEach(function(el){
-//  console.log( el );
-//});
-
-
-getKalturaData('0_gq6w4m8p');
-*/
-/*
-require('./kaltura/kWidget.getSources.js');
-kWidget.getSources({
-  //wid = '_' + 2376541;
-  partnerId: 2376541,
-  //callback = 'sourcesReady';
-});
-*/

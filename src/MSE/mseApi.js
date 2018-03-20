854f32ea368da0e93838b6f8c16dd382afe9847e
Aleksandar Veljkovic <aleksandar.veljkovic@diwanee.com>
2018-03-20 16:27:49 +0100

    code refactoring

--- src/MSE/mseApi.js
+++ src/MSE/mseApi.js
@@ -1,18 +1,52 @@
-export default function linkVideo(videoSrc, ext, codac){
-  var vidElement = document.createElement("video");
-  vidElement.controls = true;
-  document.body.appendChild(vidElement);
-  if (window.MediaSource || !MediaSource.isTypeSupported('video/webm; codecs="vp8,vorbis"')) {
-    var mediaSource = new MediaSource();
-    vidElement.src = URL.createObjectURL(mediaSource);
-    mediaSource.addEventListener('sourceopen', sourceOpen);
-  } else {
-    console.log("The Media Source Extensions API is not supported.")
-  }
+export default function linkVideo(flavors){
+
+  //videoSrc, ext, codac
+  flavors.forEach( flavor => {
+    let sw = flavor.fileExt+"-"+flavor.videoCodecId;
+    console.log(sw)
+
+    switch (sw) {
+      case 'webm-v_vp8' :
+        flavor.mime = 'video/webm; codecs="vp8,vorbis"'
+        break;
+      case 'mp4-avc1' :
+        flavor.mime = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+        break;
+      case '3gp-mpeg-4 visual' :
+        flavor.mime = 'video/3gpp; codecs="mp4v.20.8, samr"'
+        break;
+      case 'mp4-undefined' :
+        flavor.mime = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+        break;
+    }
+
+
+    if (window.MediaSource && MediaSource.isTypeSupported(flavor.mime)) {
+      var vidElement = document.createElement("video");
+      vidElement.controls = true;
+      document.getElementsByClassName('container')[0].insertAdjacentHTML( 'beforeend', flavor.mime );
+      document.getElementsByClassName('container')[0].appendChild(vidElement);
+      var mediaSource = new MediaSource();
+      vidElement.src = URL.createObjectURL(mediaSource);
+      mediaSource.addEventListener(  'sourceopen', function(e){
+        sourceOpen({e:e, flavor: flavor, vEl: vidElement})
+      }
+    );
+    } else {
+      console.log("The Media Source Extensions API is not supported.")
+    }
+  });
+
+
+
+
   var sourceBuffer;
-  function sourceOpen(e) {
+  function sourceOpen(data) {
+    var mime = data.flavor.mime;
+    var videoSrc = data.flavor.url;
+    var vidElement = data.vEl;
+    var e = data.e;
     URL.revokeObjectURL(vidElement.src);
-    var mime = 'video/mp4;  codecs="avc1.64001E, mp4a.40.2"';
     var mediaSource = e.target;
     sourceBuffer = mediaSource.addSourceBuffer(mime);
     // var videoUrl = '/src/video/test_dashinit.mp4';
--- src/main.js
+++ src/main.js
@@ -10,19 +10,26 @@ import linkVideo from './MSE/mseApi.js'
 
 
 const resolveKaltura = function(data, entryId) {
-  console.log('resolveKalture', data[1][0].videoCodecId);
-  console.log('resolveKalture', data);
-  data[1].map(videoFlavor => {
-    linkVideo(videoFlavor.url, videoFlavor.fileExt, videoFlavor.videoCodecId);
-    console.log( videoFlavor.fileExt, videoFlavor.videoCodecId);
-  });
+  //console.log('resolveKalture', data[1][0].videoCodecId);
+  console.log('resolveKalture',  data);
+  linkVideo(data[1]);
+
+
+
+  //data[1].map(videoFlavor => {
+  //  linkVideo(videoFlavor.url, videoFlavor.fileExt, videoFlavor.videoCodecId);
+    //console.log( videoFlavor.fileExt, videoFlavor.videoCodecId);
+  //});
   // linkVideo(data[1][0].url, data[1][0].fileExt, data[1][0].videoCodecId);
 }
 
 // ITERATE PAGE VIDEOS
 let htmlVideos = document.getElementsByTagName('video');
+getKalturaData('0_gq6w4m8p').then(resolveKaltura);
+/*
 [...htmlVideos].forEach(function(video, index){
   if ( video.classList.contains('video-player-kaltura') ){
     getKalturaData(video.dataset.vid).then(resolveKaltura);//dataset
   };
 });
+*/

a5e2d7fff3776c1c45164a522df9da779b8344e9
_vasilie <vasilije.milenkovic@diwanee.com>
2018-05-09 10:49:51 +0200

    hls support

--- src/player-template/player-template.js
+++ src/player-template/player-template.js
@@ -27,6 +27,13 @@ export default function(videoDom) {
 
   `;
 
+  var hlsPlayer = document.createElement("video");
+  hlsPlayer.controls = true;
+  hlsPlayer.src = 'http://www.streambox.fr/playlists/test_001/stream.m3u8';
+
+  wrap.appendChild(hlsPlayer);
+  hlsPlayer.play();
+
   wrap.insertAdjacentHTML("afterbegin", template);
   videoDom.insertAdjacentElement("afterend", wrap);
 
